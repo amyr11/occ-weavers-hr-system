812e12383e43773a5aa4128e503edb5f5319cc9e
Amyr Francisco <amyrfrancisco11@gmail.com>
2024-06-24 01:16:11 +0800

    fix(middleware): prevent downloading file information sheet when not logged in

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,7 +25,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('/')
+            ->path('/admin')
             ->databaseNotifications()
             ->viteTheme('resources/css/filament/admin/theme.css')
             ->login()
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,14 @@
 use App\Http\Controllers\FileInformationSheetController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return redirect(route('filament.admin.auth.login'));
+});
 
-Route::get('file-information-sheet/{employee}', FileInformationSheetController::class)->name('file-information-sheet');
+Route::get('file-information-sheet/{employee}', FileInformationSheetController::class)
+	->name('file-information-sheet')
+	->middleware('auth');
+
+Route::get('/login', function () {
+    return redirect(route('filament.admin.auth.login'));
+})->name('login');
