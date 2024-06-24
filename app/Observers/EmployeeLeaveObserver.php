f88f5e27a097a3bf74a46e99bbf938024b0a603b
Amyr Francisco <amyrfrancisco11@gmail.com>
2024-06-24 17:51:13 +0800

    refactor: add optional scheduler for leave

---
+++ app/Observers/EmployeeLeaveObserver.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\EmployeeLeave;
+
+class EmployeeLeaveObserver
+{
+    /**
+     * Handle the EmployeeLeave "created" event.
+     */
+    public function created(EmployeeLeave $employeeLeave): void
+    {
+        // Modify the current_leave_days of the employee
+        $employee = $employeeLeave->employee;
+        $employee->current_leave_days -= $employeeLeave->duration_in_days;
+        $employee->save();
+    }
+
+    /**
+     * Handle the EmployeeLeave "updated" event.
+     */
+    public function updated(EmployeeLeave $employeeLeave): void
+    {
+        //
+    }
+
+    /**
+     * Handle the EmployeeLeave "deleted" event.
+     */
+    public function deleted(EmployeeLeave $employeeLeave): void
+    {
+        //
+    }
+
+    /**
+     * Handle the EmployeeLeave "restored" event.
+     */
+    public function restored(EmployeeLeave $employeeLeave): void
+    {
+        //
+    }
+
+    /**
+     * Handle the EmployeeLeave "force deleted" event.
+     */
+    public function forceDeleted(EmployeeLeave $employeeLeave): void
+    {
+        //
+    }
+}
--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Models\Employee;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+// Schedule::call(function () {
+//     // Increment the current_leave_days of all employees by the max_leave_days
+//     $employees = Employee::all();
+//     foreach ($employees as $employee) {
+//         $employee->current_leave_days += $employee->max_leave_days;
+//         $employee->save();
+//     }
+// })
+//     ->yearly();
