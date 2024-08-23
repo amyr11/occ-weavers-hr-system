c36631e527762af532712377ac47d40653dff898
amyr11 <amyrfrancisco11@gmail.com>
2024-08-23 15:06:17 +0800

    refactor: Update logo.blade.php to add more style

--- resources/views/filament/admin/logo.blade.php
+++ resources/views/filament/admin/logo.blade.php
@@ -1,4 +1,7 @@
 <div class="flex items-center gap-2">
 	<img src="{{ asset('/images/logo.png') }}" alt="Logo" class="h-10">
-	<span class="text-lg font-bold">{{ env('APP_NAME') }}</span>
+	<div class="flex gap-1 items-center">
+		<h1 class="font-bold">{{ env('APP_NAME') }}</h1>
+		<span class="text-xs px-1.5	 py-0.5 rounded-md border items-center text-center dark:bg-green-950 dark:border-green-700 dark:text-green-500 bg-green-100 border-green-500 text-green-600 font-medium">{{ env('APP_NAME_TAG') }}</span>
+	</div>
 </div>
