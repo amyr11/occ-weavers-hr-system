85fb5e4f83385f592ee115537a22880b88d6748f
amyr11 <amyrfrancisco11@gmail.com>
2024-08-27 23:20:32 +0800

    feat: Add Filament user interface implementation to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
 
@@ -43,5 +46,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return str_ends_with($this->email, '@occweaversltd.com');
     }
 }
