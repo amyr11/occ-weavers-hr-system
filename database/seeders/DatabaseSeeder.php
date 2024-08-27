a9a7233134de36955274798c956828bed610a6ef
amyr11 <amyrfrancisco11@gmail.com>
2024-08-27 23:16:19 +0800

    refactor: remove user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserSeeder::class,
             CountrySeeder::class,
             InsuranceClassSeeder::class,
             EducationLevelSeeder::class,
