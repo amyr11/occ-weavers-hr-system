faeff3b46e61945a2973eef7fa2270ffb699ab5c
amyr11 <amyrfrancisco11@gmail.com>
2024-09-01 03:19:24 +0800

    refactor: Update null columns for new migration

--- database/migrations/2024_08_31_190202_set_default_max_leave_days_to_employees.php
+++ database/migrations/2024_08_31_190202_set_default_max_leave_days_to_employees.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,6 +12,15 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // Update any NULL values to the default values
+        DB::table('employees')
+            ->whereNull('max_leave_days')
+            ->update(['max_leave_days' => 21]);
+
+        DB::table('employees')
+            ->whereNull('current_leave_days')
+            ->update(['current_leave_days' => 0]);
+
         Schema::table('employees', function (Blueprint $table) {
             $table->integer('max_leave_days')->default(21)->nullable(false)->change();
             $table->integer('current_leave_days')->default(0)->nullable(false)->change();
