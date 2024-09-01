86d25e3147ed3ded1cc04876bff64782e5d17722
amyr11 <amyrfrancisco11@gmail.com>
2024-09-02 02:25:18 +0800

    refactor: Change 'status' column calculation in contracts table

---
+++ database/migrations/2024_09_01_182425_change_condition_on_status_to_contracts.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('contracts', function (Blueprint $table) {
+            $table->string('status')->virtualAs('CASE 
+                WHEN CURDATE() < start_date THEN "Upcoming"
+                WHEN CURDATE() >= end_date AND CURDATE() >= paper_contract_end_date THEN "Expired (Both)"
+                WHEN CURDATE() >= end_date THEN "Expired (Electronic)"
+                WHEN CURDATE() >= paper_contract_end_date THEN "Expired (Paper)"
+                ELSE "Active"
+            END')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('contracts', function (Blueprint $table) {
+            $table->string('status')->virtualAs('CASE 
+                WHEN CURDATE() < start_date THEN "Upcoming"
+                WHEN CURDATE() >= start_date AND CURDATE() < end_date AND CURDATE() < paper_contract_end_date THEN "Active"
+                WHEN CURDATE() >= end_date AND CURDATE() >= paper_contract_end_date THEN "Expired (Both)"
+                WHEN CURDATE() >= end_date THEN "Expired (Electronic)"
+                WHEN CURDATE() >= paper_contract_end_date THEN "Expired (Paper)"
+            END')->change();
+        });
+    }
+};
